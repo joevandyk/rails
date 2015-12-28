44772b721a1285f77f786ae1d849be2d899c8121
Joe Van Dyk <joe@tanga.com>
2015-12-27 22:45:59 -0800

    Show how hstore breaks with find_or_initialize_by
      1) Error:
    PostgresqlHstoreTest#test_find_or_initialize_by:
    ActiveRecord::StatementInvalid: PG::UndefinedTable: ERROR:  missing
    FROM-clause entry for table "settings"
    LINE 1: SELECT  "hstores".* FROM "hstores" WHERE "settings"."one" = ...
                                                     ^
    : SELECT  "hstores".* FROM "hstores" WHERE "settings"."one" = $1 LIMIT $2
        activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb:628:in `prepare'
        activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb:628:in `prepare_statement'
        activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb:589:in `exec_cache'
        activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb:576:in `execute_and_clear'
        activerecord/lib/active_record/connection_adapters/postgresql/database_statements.rb:160:in `exec_query'
        activerecord/lib/active_record/connection_adapters/abstract/database_statements.rb:369:in `select_prepared'
        activerecord/lib/active_record/connection_adapters/abstract/database_statements.rb:39:in `select_all'
        activerecord/lib/active_record/connection_adapters/abstract/query_cache.rb:70:in `select_all'
        activerecord/lib/active_record/querying.rb:39:in `find_by_sql'
        activerecord/lib/active_record/relation.rb:691:in `exec_queries'
        activerecord/lib/active_record/relation.rb:572:in `load'
        activerecord/lib/active_record/relation.rb:252:in `to_a'
        activerecord/lib/active_record/relation/finder_methods.rb:487:in `find_take'
        activerecord/lib/active_record/relation/finder_methods.rb:100:in `take'
        activerecord/lib/active_record/relation/finder_methods.rb:78:in `find_by'
        activerecord/lib/active_record/relation.rb:233:in `find_or_initialize_by'

--- activerecord/test/cases/adapters/postgresql/hstore_test.rb
+++ activerecord/test/cases/adapters/postgresql/hstore_test.rb
@@ -145,6 +145,14 @@ if ActiveRecord::Base.connection.supports_extensions?
       assert_equal "GMT", y.timezone
     end
 
+    def test_find_or_initialize_by
+      x = Hstore.find_or_initialize_by(settings: { 'one' => 'two' })
+      x.save
+      y = Hstore.find_or_initialize_by(settings: { 'one' => 'two' })
+      y.save
+      assert_equal(x, y)
+    end
+
     def test_yaml_round_trip_with_store_accessors
       x = Hstore.new(language: "fr", timezone: "GMT")
       assert_equal "fr", x.language
